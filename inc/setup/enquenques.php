2e9c192bb8cb05bc01711871f9626d96fb2f935c
mpalenciap <69210348+mpalenciap@users.noreply.github.com>
2024-03-26 11:09:24 -0400

    cambios style

--- inc/setup/enquenques.php
+++ inc/setup/enquenques.php
@@ -21,7 +21,13 @@ function newasipi_scripts() {
     * );
    */
   
-    wp_enqueue_style('newasipi-style',NEWASIPI_URL.'/assets/css/custom.css',array(),NEWASIPI_VERSION,'all');
+
+
+    wp_enqueue_style('font','https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&display=swap',array(),NEWASIPI_VERSION,'all');
+
+    wp_enqueue_style('bootstrap',NEWASIPI_URL.'/assets/libs/bootstrap/css/bootstrap.min.css',array(),NEWASIPI_VERSION,'all');
+  
+    wp_enqueue_style('newasipi-style',NEWASIPI_URL.'/assets/css/custom.css',array('bootstrap'),NEWASIPI_VERSION,'all');
 
     wp_enqueue_script('newasipi-script',NEWASIPI_URL.'/assets/js/custom.js',array('jquery'),NEWASIPI_VERSION,true);
 }
