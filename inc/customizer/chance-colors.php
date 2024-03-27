ce3fce60447bb6836e5566757d72bafca4217eb8
Juan Carlos Avila <juanvs23@gmail.com>
2024-03-27 12:04:06 -0400

    tomando cambios en el menu

--- inc/customizer/chance-colors.php
+++ inc/customizer/chance-colors.php
@@ -1,20 +1,30 @@
 <?php
-add_action( 'customize_register', 'newasipi_customize_register' );
-function newasipi_customize_register( $wp_customize ) {
-    
-    $wp_customize->add_section('background_color', array(
-        'title' => __('Background Color', 'theme_name'),
+
+function newasipi_customize_register_colors( $wp_customize ) {
+    #añadir panel al customizer
+    $wp_customize->add_panel( 'theme_colors_panel', array(
+        'title' => __( 'Colores del Tema', 'your_theme_textdomain' ),
         'priority' => 30,
-    ));
+    ) );
 
-    $wp_customize->add_setting('body_background_color', array(
-        'default' => '#ffffff',
+    // Agregar una sección para los colores principales
+    $wp_customize->add_section( 'primary_colors_section', array(
+        'title' => __( 'Colores Principales', 'your_theme_textdomain' ),
+        'panel' => 'theme_colors_panel',
+        'priority' => 10,
+    ) );
+
+    // Agregar un control de color para el color principal 1
+    $wp_customize->add_setting( 'primary_color_1', array(
+        'default' => '#002B80',
         'sanitize_callback' => 'sanitize_hex_color',
-    ));
+    ) );
 
-    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'body_background_color', array(
-        'label' => __('Background Color', 'theme_name'),
-        'section' => 'background_color',
-        'settings' => 'body_background_color',
-    )));
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'primary_color_1_control', array(
+        'label' => __( 'Color Principal 1', 'your_theme_textdomain' ),
+        'section' => 'primary_colors_section',
+        'settings' => 'primary_color_1',
+    ) ) );
 }
+
+add_action( 'customize_register', 'newasipi_customize_register_colors' );
