519c4e9c2546cfe48f0546bf0e45f69ff24c09da
Wilder Wilches <wilderwil@gmail.com>
2024-04-16 17:29:39 -0400

    Cambios en el footer  y actualizzar css

--- header.php
+++ header.php
@@ -5,60 +5,78 @@
 $add_classes = get_query_var( 'classes');
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="<?php echo do_action('newasipi_html_classes','test'); ?>" >
+<html <?php language_attributes(); ?> class="<?php echo do_action('newasipi_html_classes','test'); ?>">
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<script src=<?php NEWASIPI_PATH ?>'/wp-includes/js/jquery/jquery.min.js?ver=3.7.1' crossorigin="anonymous"></script>
-	<?php wp_head(); ?>
-	<style>
-		/* General */
-		:root {
-			--border-radius: 0;
-			--border-radius2x: 0;
-			--default: #777;
-		}
-		/* Skin Colors */
-		:root {
-			--primary: <?php echo get_theme_mod( 'primary_setting','#002B80'); ?>;
-			--primary-100: <?php echo get_theme_mod( 'primary_100_setting', '#088dc4');?>;
-			--primary-200: <?php echo get_theme_mod( 'primary_200_setting', '#0784b7');?>;
-			--primary-200: <?php echo get_theme_mod( 'primary_200_setting', '#0784b7');?>;
-			--primary-300: <?php echo get_theme_mod( 'primary_300_setting', '#077cab');?>;
-			--primary--100: <?php echo get_theme_mod( 'primary__100_setting', '#0ab1f4');?>;
-			--primary--200: <?php echo get_theme_mod( 'primary__200_setting', '#16b5f5');?>;
-			--primary--300: <?php echo get_theme_mod( 'primary__300_setting', '#22b9f6');?>;
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <script src=<?php NEWASIPI_PATH ?>'/wp-includes/js/jquery/jquery.min.js?ver=3.7.1' crossorigin="anonymous"></script>
+    <?php wp_head(); ?>
+    <style>
+    /* General */
+    :root {
+        --border-radius: 0;
+        --border-radius2x: 0;
+        --default: #777;
+    }
 
-			<?php
-			$primary_degrade = get_theme_mod( 'primary_color_degradate_setting', '#099fdc' );
-			echo degradate_color($primary_degrade,'--primary-');
-			?>
-			--secondary:  <?php echo get_theme_mod( 'secondary_setting','#068439'); ?>;
-			--secondary-100: <?php echo get_theme_mod( 'secondary_100_setting','#045ee9'); ?>;
-			--secondary-200: <?php echo get_theme_mod( 'secondary_200_setting','#0459dc'); ?>;
-			--secondary-300: <?php echo get_theme_mod( 'secondary_300_setting','#0354d0'); ?>;
-			--secondary--100: <?php echo get_theme_mod( 'secondary__100_setting','#2479fb'); ?>;
-			--secondary--200: <?php echo get_theme_mod( 'secondary__200_setting','#3181fc'); ?>;
-			--secondary--300: <?php echo get_theme_mod( 'secondary__300_setting','#3d89fc'); ?>;
-			<?php
-			$secondary_degrade = get_theme_mod( 'secondary_color_degradate_setting', '#0b6afb' );
-			echo degradate_color($secondary_degrade,'--secondary-');
-			?>
-			--tertiary:  <?php echo get_theme_mod( 'tertiary_setting','#262c36'); ?>;
-			--tertiary-100: <?php echo get_theme_mod( 'tertiary_100_setting','#1b2027'); ?>;
-			--tertiary-200: <?php echo get_theme_mod( 'tertiary_200_setting','#161a20'); ?>;
-			--tertiary-300: <?php echo get_theme_mod( 'tertiary_300_setting','#111418'); ?>;
-			--tertiary--100: <?php echo get_theme_mod( 'tertiary__100_setting','#313845'); ?>;
-			--tertiary--200: <?php echo get_theme_mod( 'tertiary__200_setting','#363e4c'); ?>;
-			--tertiary--300: <?php echo get_theme_mod( 'tertiary__300_setting','#3b445'); ?>;
-			<?php
-			$tertiary_degrade = get_theme_mod( 'tertiary_color_degradate_setting', '#262c36' );
-			echo degradate_color($tertiary_degrade,'--tertiary-');
-			?>
+    /* Skin Colors */
+    :root {
+        --primary: <?php echo get_theme_mod('primary_setting', '#002B80');
+        ?>;
+        --primary-100: <?php echo get_theme_mod('primary_100_setting', '#088dc4');
+        ?>;
+        --primary-200: <?php echo get_theme_mod('primary_200_setting', '#0784b7');
+        ?>;
+        --primary-200: <?php echo get_theme_mod('primary_200_setting', '#0784b7');
+        ?>;
+        --primary-300: <?php echo get_theme_mod('primary_300_setting', '#077cab');
+        ?>;
+        --primary--100: <?php echo get_theme_mod('primary__100_setting', '#0ab1f4');
+        ?>;
+        --primary--200: <?php echo get_theme_mod('primary__200_setting', '#16b5f5');
+        ?>;
+        --primary--300: <?php echo get_theme_mod('primary__300_setting', '#22b9f6');
+        ?>;
 
-			/*
+        <?php $primary_degrade=get_theme_mod('primary_color_degradate_setting', '#099fdc');
+        echo degradate_color($primary_degrade, '--primary-');
+        ?>--secondary: <?php echo get_theme_mod('secondary_setting', '#068439');
+        ?>;
+        --secondary-100: <?php echo get_theme_mod('secondary_100_setting', '#00953A');
+        ?>;
+        --secondary-200: <?php echo get_theme_mod('secondary_200_setting', '#0459dc');
+        ?>;
+        --secondary-300: <?php echo get_theme_mod('secondary_300_setting', '#0354d0');
+        ?>;
+        --secondary--100: <?php echo get_theme_mod('secondary__100_setting', '#2479fb');
+        ?>;
+        --secondary--200: <?php echo get_theme_mod('secondary__200_setting', '#3181fc');
+        ?>;
+        --secondary--300: <?php echo get_theme_mod('secondary__300_setting', '#3d89fc');
+        ?>;
+        <?php $secondary_degrade=get_theme_mod('secondary_color_degradate_setting', '#0b6afb');
+        echo degradate_color($secondary_degrade, '--secondary-');
+        ?>--tertiary: <?php echo get_theme_mod('tertiary_setting', '#262c36');
+        ?>;
+        --tertiary-100: <?php echo get_theme_mod('tertiary_100_setting', '#1b2027');
+        ?>;
+        --tertiary-200: <?php echo get_theme_mod('tertiary_200_setting', '#161a20');
+        ?>;
+        --tertiary-300: <?php echo get_theme_mod('tertiary_300_setting', '#111418');
+        ?>;
+        --tertiary--100: <?php echo get_theme_mod('tertiary__100_setting', '#313845');
+        ?>;
+        --tertiary--200: <?php echo get_theme_mod('tertiary__200_setting', '#363e4c');
+        ?>;
+        --tertiary--300: <?php echo get_theme_mod('tertiary__300_setting', '#3b445');
+        ?>;
+        <?php $tertiary_degrade=get_theme_mod('tertiary_color_degradate_setting', '#262c36');
+        echo degradate_color($tertiary_degrade, '--tertiary-');
+        ?>
+        /*
 			--tertiary: #262c36;
 			--tertiary-100: #1b2027;
 			--tertiary-200: #161a20;
@@ -128,56 +146,67 @@ $add_classes = get_query_var( 'classes');
 			--light-rgba-80: rgba(255, 255, 255, 0.8);
 			--light-rgba-90: rgba(255, 255, 255, 0.9);
 			*/
-			
-		}
-		/* Skin Colors - Inverse */
-		:root {
-			--primary-inverse: <?php echo get_theme_mod( 'primary_inverse_setting','#FFF'); ?>;
-			--secondary-inverse:  <?php echo get_theme_mod( 'secondary_inverse_setting','#FFF'); ?>;
-			--tertiary-inverse: <?php echo get_theme_mod( 'tertiary_inverse_setting','#FFF'); ?>;
-			--quaternary-inverse: #FFF;
-			--dark-inverse: #FFF;
-			--light-inverse: #777;
-		}
-		/* Grey Colors */
-		:root {
-			--grey: #969696;
-			--grey-100: #f4f4f4;
-			--grey-200: #eaeaea;
-			--grey-300: #e5e5e5;
-			--grey-400: #e0e0e0;
-			--grey-500: #dbdbdb;
-			--grey-600: #cecece;
-			--grey-700: #c1c1c1;
-			--grey-800: #a8a8a8;
-			--grey-900: #8e8e8e;
-			--grey-1000: #757575;
-		}
-		.btn-gradient-verde:not(.btn-outline) {
-			transition: 0.3s ease all; 
-    		background: linear-gradient(135deg, <?php echo get_theme_mod( 'first_degrade_color_setting','#00953A'); ?>  0%, <?php echo get_theme_mod( 'second_degrade_color_setting','#008036'); ?>  80%) !important;
-    		color: var(--primary-inverse);
-		}
-		.btn-gradient-verde:not(.btn-outline):hover, .btn-gradient-verde:not(.btn-outline).hover {
-   			background: linear-gradient(135deg,  <?php echo get_theme_mod( 'first_degrade_color_hover_setting','#008036'); ?>  0%, <?php echo get_theme_mod( 'second_degrade_color_hover_setting','#00953A'); ?>  80%) !important;
-    		color: var(--primary-inverse);
-		}
-.btn-gradient-azul:not(.btn-outline) {
-    background: linear-gradient(135deg, <?php echo get_theme_mod( 'first_blue_degrade_color_setting','#002B80'); ?> 0%, <?php echo get_theme_mod( 'second_blue_degrade_color_setting','#4458A3'); ?> 80%) !important;
-    color: var(--primary-inverse);
-}
-.btn-gradient-azul:not(.btn-outline):hover, .btn-gradient-azul:not(.btn-outline).hover {
-    background: linear-gradient(135deg, <?php echo get_theme_mod( 'first_blue_degrade_color_hover_setting','#4458A3'); ?> 0%, <?php echo get_theme_mod( 'second_blue_degrade_color_hover_setting','#002B80'); ?>  80%) !important;
-    color: var(--primary-inverse);
-}
 
-	</style>
+    }
+
+    /* Skin Colors - Inverse */
+    :root {
+        --primary-inverse: <?php echo get_theme_mod('primary_inverse_setting', '#FFF');
+        ?>;
+        --secondary-inverse: <?php echo get_theme_mod('secondary_inverse_setting', '#FFF');
+        ?>;
+        --tertiary-inverse: <?php echo get_theme_mod('tertiary_inverse_setting', '#FFF');
+        ?>;
+        --quaternary-inverse: #FFF;
+        --dark-inverse: #FFF;
+        --light-inverse: #777;
+    }
+
+    /* Grey Colors */
+    :root {
+        --grey: #969696;
+        --grey-100: #eaeaea;
+        --grey-200: #f4f4f4;
+        --grey-300: #e5e5e5;
+        --grey-400: #e0e0e0;
+        --grey-500: #dbdbdb;
+        --grey-600: #cecece;
+        --grey-700: #c1c1c1;
+        --grey-800: #a8a8a8;
+        --grey-900: #8e8e8e;
+        --grey-1000: #757575;
+    }
+
+    .btn-gradient-verde:not(.btn-outline) {
+        transition: 0.3s ease all;
+        background: linear-gradient(135deg, <?php echo get_theme_mod('first_degrade_color_setting', '#00953A'); ?> 0%, <?php echo get_theme_mod('second_degrade_color_setting', '#008036'); ?> 80%) !important;
+        color: var(--primary-inverse);
+    }
+
+    .btn-gradient-verde:not(.btn-outline):hover,
+    .btn-gradient-verde:not(.btn-outline).hover {
+        background: linear-gradient(135deg, <?php echo get_theme_mod('first_degrade_color_hover_setting', '#008036'); ?> 0%, <?php echo get_theme_mod('second_degrade_color_hover_setting', '#00953A'); ?> 80%) !important;
+        color: var(--primary-inverse);
+    }
+
+    .btn-gradient-azul:not(.btn-outline) {
+        background: linear-gradient(135deg, <?php echo get_theme_mod('first_blue_degrade_color_setting', '#002B80'); ?> 0%, <?php echo get_theme_mod('second_blue_degrade_color_setting', '#4458A3'); ?> 80%) !important;
+        color: var(--primary-inverse);
+    }
+
+    .btn-gradient-azul:not(.btn-outline):hover,
+    .btn-gradient-azul:not(.btn-outline).hover {
+        background: linear-gradient(135deg, <?php echo get_theme_mod('first_blue_degrade_color_hover_setting', '#4458A3'); ?> 0%, <?php echo get_theme_mod('second_blue_degrade_color_hover_setting', '#002B80'); ?> 80%) !important;
+        color: var(--primary-inverse);
+    }
+    </style>
 </head>
+
 <body data-plugin-page-transition <?php body_class( 'bg-white text-gray-900 antialiased '. $add_classes ); ?>>
-<?php do_action('newasipi_site_before'); ?>
-<?php do_action('newasipi_site_header');?>
+    <?php do_action('newasipi_site_before'); ?>
+    <?php do_action('newasipi_site_header');?>
 
 
-<!--menu here-->
+    <!--menu here-->
 
-<?php echo get_template_part('template-parts/header/header-content'); ?>    
+    <?php echo get_template_part('template-parts/header/header-content'); ?>
--- template-parts/header/header-logo.php
+++ template-parts/header/header-logo.php
@@ -2,16 +2,16 @@
     <div class="header-row">
         <div class="header-logo">
             <?php if ( has_custom_logo() ) { ?>
-                <a href="<?php echo get_bloginfo( 'url' ); ?>" class="text-lg font-extrabold uppercase">
-                    <img class="w-full h-auto" src="<?php echo get_custom_logo_url(); ?>" alt="<?php echo get_bloginfo( 'name' ); ?>" srcset="">
-                </a>
+            <a href="<?php echo get_bloginfo( 'url' ); ?>" class="text-lg font-extrabold uppercase">
+                <img src="<?php echo get_custom_logo_url(); ?>" alt="<?php echo get_bloginfo( 'name' ); ?>" srcset="">
+            </a>
             <?php } else { ?>
-                <a href="<?php echo get_bloginfo( 'url' ); ?>" class="text-lg font-extrabold uppercase">
-                    <?php echo get_bloginfo( 'name' ); ?>
-                </a>
-                <p class="text-sm font-light text-gray-600">
-                    <?php echo get_bloginfo( 'description' ); ?>
-                </p>
+            <a href="<?php echo get_bloginfo( 'url' ); ?>" class="text-lg font-extrabold uppercase">
+                <?php echo get_bloginfo( 'name' ); ?>
+            </a>
+            <p class="text-sm font-light text-gray-600">
+                <?php echo get_bloginfo( 'description' ); ?>
+            </p>
             <?php } ?>
         </div>
     </div>
